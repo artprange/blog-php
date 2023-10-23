a5e903a2b8956952ea842ee1d32be815dde9a79c
xprangex <arthur.prange@gmail.com>
2023-10-23 11:16:40 -0300

    implementing search queries

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,22 +1,45 @@
 <div class="col-md-4">
 
+
+
+
+
+
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
+
+    <form action="search" method="post">
     <div class="input-group">
-        <input type="text" class="form-control">
+        <input name="search.php" type="text" class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
+            <button name="submit" class="btn btn-default" type="submit">
                 <span class="glyphicon glyphicon-search"></span>
         </button>
         </span>
     </div>
+    </form>  <!-- search form -->
+
+
     <!-- /.input-group -->
 </div>
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- Blog Categories Well -->
 <div class="well">
     <h4>Blog Categories</h4>
+
+        
     <div class="row">
         <div class="col-lg-6">
             <ul class="list-unstyled">
